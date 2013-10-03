caceea05156a9af2e00af9790e51b7946b3bf444
encode101 <rahulmishra.hello@gmail.com>
2013-10-04 02:24:06 +0530

    version 1.0 > under development

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+jQuery(function(){
+	
+	$("#app").html(defaultHome());
+	
+	function defaultHome(){
+		return "<h1>Hello World</h1><p>Something meaningful comes here</p>";
+	}
+})
