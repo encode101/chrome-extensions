268a239ba53813ec83da16465f165194bf24a9eb
encode101 <rahulmishra.hello@gmail.com>
2013-10-04 03:50:23 +0530

    Developmer Interim Commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,17 @@ jQuery(function(){
 // allowed create notifications.
 
 // Create a simple text notification:
-var notification = webkitNotifications.createNotification(
-  'icon.png',  // icon url - can be relative
-  'Hello!',  // notification title
-  'Your Message To +91-9545600524, Just Got Delivered'  // notification body text
-);
+	/*var notification = webkitNotifications.createNotification(
+	  'img/icon.png',  // icon url - can be relative
+	  'Hello!',  // notification title
+	  'Your Message To +91-9545600524, Just Got Delivered'  // notification body text
+	);*/
+	
+	
 
 // Then show the notification.
-	setInterval(function(){
+	
+		
+		var notification = webkitNotifications.createNotification();
 		notification.show();
-	},2000);
-
 })
