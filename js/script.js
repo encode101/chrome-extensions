1702d8b71ba2da3033a116ff6e82e2c36cc8d3ff
encode101 <rahulmishra.hello@gmail.com>
2013-10-04 03:00:03 +0530

    Developmer Interim Commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 // JavaScript Document
 
 jQuery(function(){
-	
-	$("#app").html(defaultHome());
-	
-	function defaultHome(){
-		return "<h1>Hello World</h1><p>Something meaningful comes here</p>";
-	}
+	// Note: There's no need to call webkitNotifications.checkPermission().
+// Extensions that declare the notifications permission are always
+// allowed create notifications.
+
+// Create a simple text notification:
+var notification = webkitNotifications.createNotification(
+  'icon.png',  // icon url - can be relative
+  'Hello!',  // notification title
+  'Your Message To +91-9545600524, Just Got Delivered'  // notification body text
+);
+
+// Then show the notification.
+	setInterval(function(){
+		notification.show();
+	},2000);
+
 })
